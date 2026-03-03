99d4f1cb144d8a64cbcdda0dd42f7f7788a0ef3c
Muhammad Naufal Fadhilah <117992655+nopalllfd@users.noreply.github.com>
2026-03-03 13:27:09 +0700

    Create minitask-2.js

---
+++ minitask-2.js
@@ -0,0 +1,13 @@
+const dateConvertion = async (date) => {
+  const regex = /^(\d{1,2})\-(\d{1,2})\-(\d{4})$/;
+  const validate = regex.test(date);
+  if (!validate) {
+    return `Format tanggal salah`;
+  } else {
+    const split = date.split('-');
+    const res = split.join('/');
+    return `Hasil dari konversi adalah ${res}`;
+  }
+};
+
+export default dateConvertion;
