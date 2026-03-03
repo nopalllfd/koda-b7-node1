c8cf5b3fb4306b54c152d1f505b6b4b48813b5ce
Muhammad Naufal Fadhilah <117992655+nopalllfd@users.noreply.github.com>
2026-03-03 12:00:16 +0700

    Update minitask-1.js

--- minitask-1.js
+++ minitask-1.js
@@ -1,17 +1,15 @@
 import moment from 'moment';
 
-const date = '20-04-2020';
-
 const dateConvertion = (date) => {
   const validate = moment(date, 'DD-MM-YYYY', true).isValid();
   console.log(`tanggal dimasukkan ${date}`);
   if (!validate) {
     return `Format tanggal salah`;
   } else {
-    const res = moment(date, 'DD/MM/YY');
+    const res = moment(date, 'DD/MM/YYYY');
     const convertedDate = res.format('DD/MM/YYYY');
     return `Hasil konversi adalah ${convertedDate}`;
   }
 };
 
-console.log(dateConvertion(date));
+export default dateConvertion;
